d7249f0074700ed46156048bc651b34044f9f332
akash-suresh <akashsuresh36@gmail.com>
2015-05-25 01:01:49 +0530

    corrections on bnb

--- traveling_salesman.c
+++ traveling_salesman.c
@@ -71,14 +71,12 @@ int choose(int n, int *temp[], int *arr[], int u, int i, int low_bound){
 		temp[j][i]=1000;
 	}
 	temp[i][0]=1000;
-	//display(n,temp);
 	nlb=mat_reduce(n,temp);
-	//printf("\nnlb is %d",nlb);
-	//display(n,temp);
 	cost = low_bound + arr[u][i] + nlb;
 	return cost;
 }
-int BandB(int n, int *arr[]){
+/*
+int BandB1(int n, int *arr[]){
 	int i,j,u,low_bound,p,nlb,visited[n],order_visit[n],k=0,*min_arr[100],*temp[100],cost,l,m,min_cost,min_cost_index;
 	for(i=0;i<n;i++){
 		visited[i]=0;
@@ -87,10 +85,8 @@ int BandB(int n, int *arr[]){
 	u=0; //previous node. initially it is source node
 	//calculated lower bound at node 0
 	low_bound = mat_reduce(n,arr);
-	//printf("original lower bound %d",low_bound);
-	order_visit[k++]=0;
+	//order_visit[k++]=0;
 	visited[u]=1;
-	//display(n,arr);
     for(i=0; i<n; i++){
         temp[i] = (int*)calloc(n,sizeof(int));
         min_arr[i]= (int*)calloc(n,sizeof(int));
@@ -103,7 +99,6 @@ int BandB(int n, int *arr[]){
 			if(visited[i]!=1){
 				copy(n,arr,temp);
 				cost = choose(n,temp,arr,u ,i, low_bound);
-				//printf("\ncost of node %d to %d is %d" ,u,i,cost);
 				if(cost<min_cost){
 					min_cost=cost;
 					min_cost_index=i;
@@ -112,33 +107,89 @@ int BandB(int n, int *arr[]){
 			}
 		}
 		u=min_cost_index;
-		//printf("\n\ngoing to %d ",u);
 		visited[u]=1;
 		order_visit[k++]=u;
 		low_bound=min_cost;
 		copy(n,min_arr,arr);
-		//printf("\n\narray for next is ");
-		//display(n,arr);
 	}
-	//to display order of visit
-    /*printf("\norder of visit is ");
-    for(i=0;i<n;i++){
-    	printf("%d ",order_visit[i]);
-    }
-    printf("\n total cost is %d\n",low_bound);
-*/	return low_bound;
+	return low_bound;
 }
+*/
+int BandB(int n, int *arr[], int visited[], int u){
+	int i,k,x,y,count=0,*temp[n],low_bound,next[n], min_cost, min_cost_index,cost;
+	
+	low_bound=mat_reduce(n,arr);
+	visited[u]=1;
+	//printf("\nlow bound is %d",low_bound);
+	//display(n,arr);
+	for(i=0;i<n;i++){
+		if(visited[i]==1){
+			count++;
+		}
+	}
+	if(count==n){
+		return 0;
+	}
+    
+    for(i=0; i<n; i++){
+    	next[i]=0;
+        temp[i] = (int*)calloc(n,sizeof(int));
+    }
 
+	min_cost=1000;
+	for(i=1;i<n;i++){
+		if(visited[i]!=1){
+			copy(n,arr,temp);
+			cost = choose(n,temp,arr,u,i,low_bound);
+	//		printf("\ncost for %d is %d",i,cost);
+			if(cost<min_cost){
+	//			printf("\ncost %d is replaced by %d",min_cost,cost);
+				min_cost=cost;
+				min_cost_index=i;
+				for(k=0;k<n;k++){
+					next[k]=0;
+				}
+				next[i]=1;
+			}
+			else if(cost==min_cost){
+				next[i]=1;
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		if(next[i]==1){
+			copy(n,arr,temp);
+			cost = choose(n,temp,arr,u,i,low_bound);
+	//		printf("\ngoing to %d",i);
+
+			x = BandB(n,temp,visited,i);		
+			x = x + cost;
+			break;
+		}
+	}
+	i++;
+	for(;i<n;i++){
+		if(next[i]==1){
+			copy(n,arr,temp);
+			cost = choose (n,temp,arr,u,i,low_bound);
+			y= BandB(n,temp,visited,i);
+			y = y + cost;
+			if(y<x){
+				x=y;
+			}
+		}
+	}
+	return x;
+
+}
 int greedy(int n, int *arr[]){
-	int i,j,visited[n],u,cost=0,min_dist,min_ind,k=0,order_visit[n];
+	int i,j,visited[n],u,cost=0,min_dist,min_ind;
 
 	for(i=0;i<n;i++){
 		visited[i]=0;
 	}
 	u=0; //initially source node is 0
 	visited[0]=1;
-	order_visit[k++]=0;
-
 	for(i=1;i<n;i++){
 		min_dist=1000;
 		for(j=1;j<n;j++){
@@ -150,29 +201,27 @@ int greedy(int n, int *arr[]){
 			}
 		}
 		u=min_ind;
-		cost+=min_dist;
+		if(min_dist!=1000){
+			cost+=min_dist;
+		}
 		visited[u]=1;
-		order_visit[k++]=u;
 	}
 	cost+=arr[u][0];
-	//to display order of visit
-    /*printf("\norder of visit is ");
-    for(i=0;i<n;i++){
-    	printf("%d ",order_visit[i]);
-    }*/
     return cost;
 }
 int main()
 {
 	clock_t start_t,end_t,total_t;
-    int *arr[100], *temp[100], x, n, m, i ,j, k, result[46][6];
+    int *arr[100], *temp[100], x, n, m, i ,j, k, result[46][6],visited[100];
     time_t t;
     srand((unsigned) time(&t));
 
     printf("\nEnter the number of nodes : ");
     scanf("%d",&n);
     for(i=0; i<n; i++){
+    	visited[i]=0;
         arr[i] = (int*)calloc(n,sizeof(int));
+        temp[i] = (int*)calloc(m,sizeof(int));
         for(j=0;j<n;j++){
         	if(i==j){
         		arr[i][j]=1000;
@@ -182,18 +231,29 @@ int main()
         		arr[i][j]=x;
         	}
         }
-    }
-
+    }/*
+    arr[0][0]=1000,arr[0][1]=20,arr[0][2]=30,arr[0][3]=10,arr[0][4]=11,arr[0][5]=40;
+    arr[1][0]=15,arr[1][1]=1000,arr[1][2]=16,arr[1][3]=4,arr[1][4]=2,arr[1][5]=42;
+    arr[2][0]=3,arr[2][1]=5,arr[2][2]=1000,arr[2][3]=2,arr[2][4]=4,arr[2][5]=11;
+    arr[3][0]=17,arr[3][1]=6,arr[3][2]=18,arr[3][3]=1000,arr[3][4]=3,arr[3][5]=20;
+    arr[4][0]=16,arr[4][1]=4,arr[4][2]=7,arr[4][3]=16,arr[4][4]=1000,arr[4][5]=4;
+    arr[5][0]=23,arr[5][1]=23,arr[5][2]=13,arr[5][3]=40,arr[5][4]=27,arr[5][5]=1000;
+*/
+  /*  
+    arr[0][0]=1000,arr[0][1]=48,arr[0][2]=4,arr[0][3]=47,arr[0][4]=23;
+    arr[1][0]=32,arr[1][1]=1000,arr[1][2]=7,arr[1][3]=33,arr[1][4]=42;
+    arr[2][0]=18,arr[2][1]=10,arr[2][2]=1000,arr[2][3]=15,arr[2][4]=7;
+    arr[3][0]=25,arr[3][1]=12,arr[3][2]=6,arr[3][3]=1000,arr[3][4]=20;
+    arr[4][0]=19,arr[4][1]=9,arr[4][2]=40,arr[4][3]=27,arr[4][4]=1000;
+*/
     display(n, arr);
     
-    for(i=0;i<46;i++){
+    for(i=0;i<=n-5;i++){
     	m=i+5;
     	for(j=0;j<m;j++){
-    		temp[j] = (int*)calloc(m,sizeof(int));
-    		for(k=0;k<m;k++){
-    			temp[j][k]=arr[j][k];
-    		}
+    		visited[j]=0;
     	}
+    	copy(m,arr,temp);	
     	start_t = clock();
     	result[i][0] = greedy(m,temp);
     	end_t = clock();
@@ -202,7 +262,7 @@ int main()
 
 
     	start_t = clock();
- 		result[i][2] = BandB(m,temp);
+ 		result[i][2] = BandB(m, temp, visited, 0);
     	end_t = clock();    	
    		total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC * 1000000;
  		result[i][3]=total_t;
@@ -210,14 +270,23 @@ int main()
  	printf("\n\n NOTE time is in microseconds");
  	printf("\n\n %25s  %20s ","GREEDY","BRANCH and BOUND");
  	printf("\n %15s  %10s  %10s  %10s\n","cost","time","cost","time");
- 	for(i=0;i<46;i++){
+ 	for(i=0;i<=n-5;i++){
  		m=i+5;
  		printf("%5d",i+5);
  		for(j=0;j<4;j++){
  			printf(" %10d ",result[i][j]);
- 		}
+ 		}/*
+ 		if(result[i][0]<result[i][2]){
+ 			printf(" boo");
+ 		}*/
  		printf("\n");
  	}
-
+/*
+    for(j=0;j<n;j++){
+    		visited[j]=0;
+    }
+ 	display(n,arr);
+ 	printf("Band B is %d ",BandB(n,arr,visited,0));
+*/
     return 0;
 }
